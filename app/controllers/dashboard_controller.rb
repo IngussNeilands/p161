62cd2f8308cba5e257a64c55dc51518fc2032401
Csaba Nagy <icnagy@gmail.com>
2015-05-20 17:02:24 +0200

    make recent updates dynamic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < SecuredController
   def index
+    @recently_happened = Message.order('created_at desc')
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,10 @@ class Message < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
   validates :owner_id, :body, presence: true, allow_nil: false, allow_blank: false
   validates :body, length: { maximum: 160 }
+
+  before_validation :sanitize_input
+
+  def sanitize_input
+    body.gsub!(/\s/,'')
+  end
 end
